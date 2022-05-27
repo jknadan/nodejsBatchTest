78300a7cf27eb1f63e31c84828857a89c15ba2af
jknadan <jknadan@gmail.com>
2022-05-27 10:47:23 +0900

    Before remove public folder

--- app.js
+++ app.js
@@ -6,8 +6,10 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+// express 패키지를 변수에 담음 → REST API 작성 가능 (그 중에서도 HTTP 4가지 메소드를 통해 URL에 접근할 수 있게 해줌)
 var app = express();
 
+// app.use ~~ = 미들웨어 사용
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// 첫번째 param = URL 정의 || 두번째 param = 해당 URL에서 할 작업 → req: URL에 담긴 요청정보 res: 작업 수행 후 보내줄 정보
+router.get('/index', function(req, res, next) {
+  const result = {
+    message : 'hello',
+    id : "123"
+  };
+
+  res.send(result)
 });
 
 module.exports = router;
